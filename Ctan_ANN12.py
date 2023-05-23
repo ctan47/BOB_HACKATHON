39dfb010877e0ce09852564f893a96e0e396435b
Chetan Hatkar <44762312+ctan47@users.noreply.github.com>
2023-05-24 00:21:09 +0530

    Add files via upload

---
+++ Ctan_ANN12.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+# Define the CNN model
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+
+model.compile(optiizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
+
+
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
+
+_, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
+print("Test Accuracy:", test_accuracy)
