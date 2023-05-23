51ce20de869360aaa0396cb4da91ef646047d4b0
Chetan Hatkar <44762312+ctan47@users.noreply.github.com>
2023-05-24 01:38:29 +0530

    Update Ctan_ANN5.py

--- Ctan_ANN5.py
+++ Ctan_ANN5.py
@@ -1,15 +1,29 @@
 import numpy as np
-X = np.array([[1, 1, 1, -1], [-1, -1, 1, 1]])
-Y = np.array([[1, -1], [-1, 1]])
-W = np.dot(Y.T, X)
 
-def bam(x):
-    y = np.sign(np.dot(W, x))
-    return y
+class BAM:
+    def __init__(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.weights = np.zeros((input_size, output_size))
+        
+    def train(self, input_patterns, output_patterns):
+        for input_pattern, output_pattern in zip(input_patterns, output_patterns):
+            self.weights += np.outer(input_pattern, output_pattern)
+    
+    def recall(self, input_pattern):
+        output_pattern = np.sign(np.dot(input_pattern, self.weights.T))
+        return output_pattern
 
-x_test = np.array([1, -1, -1, -1])
-y_test = bam(x_test)
+# Example usage:
+input_patterns = np.array([[1, -1, 1, -1],
+                           [1, 1, -1, -1]])
+output_patterns = np.array([[1, -1],
+                            [-1, 1]])
 
+bam = BAM(input_size=4, output_size=2)
+bam.train(input_patterns, output_patterns)
 
-print("Input x:", x_test)
-print("Output y:", y_test)
+input_pattern = np.array([1, -1, 1, -1])
+output_pattern = bam.recall(input_pattern)
+print("Input Pattern:", input_pattern)
+print("Output Pattern:", output_pattern)
