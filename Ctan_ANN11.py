91aa69a44492bebf80c8ea7c5bd24a8e5e65e125
Chetan Hatkar <44762312+ctan47@users.noreply.github.com>
2023-05-24 09:11:54 +0530

    Update Ctan_ANN11.py

--- Ctan_ANN11.py
+++ Ctan_ANN11.py
@@ -1,23 +1,25 @@
 import tensorflow as tf
 
-# Define the Logistic Regression model
+# Load the MNIST dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocess the data
+x_train = x_train.reshape(-1, 784) / 255.0
+x_test = x_test.reshape(-1, 784) / 255.0
+
+# Create the logistic regression model
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Dense(1, activation='sigmoid', input_shape=(784,))
 ])
 
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = x_train.reshape(-1, 28 * 28) / 255.0
-x_test = x_test.reshape(-1, 28 * 28) / 255.0
-
-
+# Train the model
 model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
 
-
-_, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print("Test Loss:", test_loss)
 print("Test Accuracy:", test_accuracy)
